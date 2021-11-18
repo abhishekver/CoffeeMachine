d2611bf5f1c2dc55ab863a2062cab1b527a28a18
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Air.local>
2021-11-18 21:36:27 +0530

    adds readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Coffee Machine
+
+## To Run
+* Execute the main method in `CoffeeMachine` class
+* OR Execute the runTest in `CoffeeMachineTest` class
+
+### Assumptions
+* The program assumes that ingredient will be limited to the ones specified in `models\Beverage` class.
+* Other assumptions as stated in the assignment
+
+## NOTE
+* In case the program is run on Windows machine and there is `FileNotFoundException`, please try changing the file path 
+`src/test/resources/TestData.json` to `src\\test\\resources\\TestData.json`
+* To test on other data, data can be changed in the file `src/test/resources/TestData.json`
--- src/main/java/CoffeeMachine.java
+++ src/main/java/CoffeeMachine.java
@@ -1,5 +1,18 @@
-public class CoffeeMachine {
-    public static void main(String[] args) {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.InputData;
+import services.CoffeeMachineService;
+import services.CoffeeMachineServiceImpl;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) throws IOException {
+        CoffeeMachineService coffeeMachineService = new CoffeeMachineServiceImpl();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = new Scanner(new File("src/test/resources/TestData.json")).useDelimiter("\\Z").next();
+        InputData inputData = objectMapper.readValue(content, InputData.class);
+        coffeeMachineService.prepareBeverage(inputData);
     }
 }
